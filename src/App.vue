d3c5296593c8c21f918122fbd3c6a8a812a4cfc6
jspir1t <stardustzjt@gmail.com>
2021-10-07 22:37:00 +0800

    poc complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input-search
+        placeholder="Search"
+        enter-button="Search"
+        size="large"
+        @search="onSearch"
+    />
+    <div v-if="suggested===true">
+      {{'(Suggestion Below)'}}
+      <ul>
+        <li v-bind:key="item.text" v-for="item in this.suggestion">
+          {{item.text + '\t' + item.score + '\t' + item.freq}}
+        </li>
+      </ul>
+    </div>
+
+    <a-list item-layout="vertical" size="large" :data-source="results">
+      <a-list-item slot="renderItem" key="item.title" slot-scope="item">
+        <img
+            v-if="results.length > 0"
+            slot="extra"
+            width="272"
+            alt="logo"
+            :src=item._source.image_link
+        />
+        <a-list-item-meta :description="item._source.second_category + item._source.third_category">
+          <a slot="title">{{ item._source.title }}</a>
+        </a-list-item-meta>
+        <div style="margin: 0 auto; width:600px;" class="description">{{ item._source.description | ellipsis }}</div>
+      </a-list-item>
+    </a-list>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      url: 'http://localhost:61288/shopping_channel/_search',
+      results: [],
+      suggested: false,
+      suggestion: [],
+    }
+  },
+  filters:{
+    ellipsis(value){
+      if (!value) return '';
+      if (value.length > 100) {
+        return value.slice(0,100) + '...'
+      }
+      return value
+    }
+  },
+  methods: {
+    onSearch(value) {
+      this.suggested = false
+      let query = {
+        "query": {
+          "match": {
+            "title": value
+          }
+        }
+      };
+      this.axios.get(this.url, {
+        params: {
+          source: JSON.stringify(query),
+          source_content_type: 'application/json'
+        }
+      }).then((response) => {
+        console.log(response.data.hits.hits.length)
+        this.results = response.data.hits.hits
+        // if need suggestion
+        if(this.results == null || this.results.length === 0) {
+          this.suggested = true
+          let suggestionQuery = {
+            "suggest": {
+              "text" : value,
+              "title_suggest" : {
+                "term" : {
+                  "field" : "title"
+                }
+              }
+            }
+          };
+          this.axios.get(this.url, {
+            params: {
+              source: JSON.stringify(suggestionQuery),
+              source_content_type: 'application/json'
+            }
+          }).then((res) => {
+            this.suggestion = JSON.parse(JSON.stringify(res.data.suggest.title_suggest[0].options))
+            console.log(this.suggestion)
+          }).catch(function (err) {
+            console.log(err);
+          })
+        }
+      }).catch(function (error) {
+        console.log(error);
+      })
+    }
   }
 }
 </script>
